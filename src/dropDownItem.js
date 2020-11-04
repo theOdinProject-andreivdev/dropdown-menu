9226eef71ecd4bbed9d0d7c6376ba3a081fd0630
Andrei Voinea <andreivdev@gmail.com>
2020-11-04 20:38:28 +0200

    class reuse example

--- src/dropDownItem.js
+++ src/dropDownItem.js
@@ -1,17 +1,17 @@
 export default class DropDownItem {
-  constructor(_element) {
+  constructor(_element, _animation) {
     this.element = _element;
+    this.animation = _animation;
     this.hide();
   }
 
   hide() {
     this.element.style.opacity = "0";
-    this.element.classList.remove("dropDownMenuItemAnimate");
+    this.element.classList.remove(this.animation);
   }
 
   show() {
     this.element.style.opacity = "1";
-    this.element.classList.add("dropDownMenuItemAnimate");
-    console.log("animate");
+    this.element.classList.add(this.animation);
   }
 }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ let dropDownMenu = new DropDownMenu(dropDownMenuElement);
 let dropDownItemElements = document.querySelectorAll(".dropDownItem");
 
 dropDownItemElements.forEach((element) => {
-  let item = new DropDownItem(element);
+  let item = new DropDownItem(element, "dropDownMenuItemAnimate");
   dropDownMenu.addItem(item);
+});
+
+//this is how you reuse it
+let anotherdropDownMenuElement = document.querySelector(".anotherdropDownMenu");
+let anotherdropDownMenu = new DropDownMenu(anotherdropDownMenuElement);
+
+let anotherdropDownItemElements = document.querySelectorAll(
+  ".anotherdropDownItem"
+);
+
+anotherdropDownItemElements.forEach((element) => {
+  let item = new DropDownItem(element, "anotherdropDownMenuItemAnimate");
+  anotherdropDownMenu.addItem(item);
 });
